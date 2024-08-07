7a7a3f022f0742b69b4fc8d4ffa8cf5a4dd3db42
Sathish Kumar Manoharan <samanoha@microsoft.com>
2024-08-07 15:21:30 -0700

    Added two stack aproach

--- arrays/count_interesting_subarrays.py
+++ arrays/count_interesting_subarrays.py
@@ -1,22 +1,42 @@
+from typing import Counter, List
+
+"""
+Intuition
+We don't care the value of A[i],
+we care if A[i] % mod == k.
+
+So if A[i] % mod == k,
+we take A[i] as 1,
+otherwise 0.
+
+
+Explanation
+we calculate the prefix sum acc of A,
+then acc mean the number of A[i] % mod == k in i + 1 first elements.
+
+count is a hashmap,
+where count[v] means the number of prefix array that have acc % mod == k.
+and we initial count[0] = 1 for empty prefix subarray.
+
+Then we iterate a in A,
+and we update prefix sum acc,
+and update increment res by count[(acc - k) % mod].
+
+Finally return res
+
+
+Complexity
+Time O(n)
+Space O(mod)
+https://leetcode.com/problems/count-of-interesting-subarrays/
+
+"""
 class Solution:
-    def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
-        return self.atMost(nums, modulo, k) - self.atMost(nums, modulo, k-1)
-    
-    def atMost(self, nums, modulo, k):
-        count = 0
-        
-        if not nums:
-            return count
-        
-        start, window_size = 0, 0
-        
-        for end in range(len(nums)):
-            window_size += 1 if nums[end] % modulo == k else 0
-            
-            while window_size > k:
-                window_size -= 1 if nums[end] % modulo == k else 0
-                start += 1
-                
-            count += end - start + 1
-        
-        return count
+    def countInterestingSubarrays(self, nums: List[int], mod: int, k: int) -> int:
+        res = acc = 0
+        count = Counter({0: 1})
+        for num in nums:
+            acc = (acc + (1 if num % mod == k else 0)) % mod
+            res += count[(acc - k) % mod]
+            count[acc] += 1
+        return res
--- stack/min_stack.py
+++ stack/min_stack.py
@@ -1,3 +1,10 @@
+"""
+https://leetcode.com/problems/min-stack/
+
+Time Complexity : O(1) for all operations.
+
+Space Complexity : O(n), where 𝑛 be the total number of operations performed.
+"""
 class MinStack:
 
     def __init__(self):
@@ -20,6 +27,56 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.stack[-1][1]
+    
+"""
+Improved two stack solution
+
+Time Complexity : O(1) for all operations.
+
+Space Complexity : O(n), where 𝑛 be the total number of operations performed.
+
+"""
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x: int) -> None:
+
+        # We always put the number onto the main stack.
+        self.stack.append(x)
+
+        # If the min stack is empty, or this number is smaller than
+        # the top of the min stack, put it on with a count of 1.
+        if not self.min_stack or x < self.min_stack[-1][0]:
+            self.min_stack.append([x, 1])
+
+        # Else if this number is equal to what's currently at the top
+        # of the min stack, then increment the count at the top by 1.
+        elif x == self.min_stack[-1][0]:
+            self.min_stack[-1][1] += 1
+
+    def pop(self) -> None:
+
+        # If the top of min stack is the same as the top of stack
+        # then we need to decrement the count at the top by 1.
+        if self.min_stack[-1][0] == self.stack[-1]:
+            self.min_stack[-1][1] -= 1
+
+        # If the count at the top of min stack is now 0, then remove
+        # that value as we're done with it.
+        if self.min_stack[-1][1] == 0:
+            self.min_stack.pop()
+
+        # And like before, pop the top of the main stack.
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1][0]
 
 
 # Your MinStack object will be instantiated and called as such:
