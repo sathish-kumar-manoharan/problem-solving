413ce700c0685c811727026ffe7b11d9153bd224
Sathish Kumar Manoharan <samanoha@microsoft.com>
2024-10-11 19:50:08 -0700

    merge intervals

---
+++ arrays/merge_intervals.py
@@ -0,0 +1,19 @@
+from typing import List
+"""
+"""
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        
+        intervals.sort(key=lambda x: x[0])
+        
+        merged = []
+        
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(interval[1], merged[-1][1])
+        
+        return merged
