ecd0c46da20350fa08d7198f56750e7a2172eb57
Sathish Kumar Manoharan <samanoha@microsoft.com>
2025-01-16 22:04:19 -0800

    Added code to find the longest consecutive sequence count

---
+++ set/longest_consecutive_sequence.py
@@ -0,0 +1,64 @@
+from typing import List
+
+"""
+https://leetcode.com/problems/longest-consecutive-sequence/
+
+"""
+
+class Solution:
+    # Time: O(N^3)
+    # Space: O(1)
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest_streak = 0
+
+        for num in nums:
+            current_num = num
+            current_streak = 1
+
+            while current_num + 1 in nums:
+                current_num += 1
+                current_streak += 1
+
+            longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
+    
+    # Time: O(N log N)
+    # Space: O(N)
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        nums.sort()
+
+        longest_streak = 1
+        current_streak = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                if nums[i] == nums[i - 1] + 1:
+                    current_streak += 1
+                else:
+                    longest_streak = max(longest_streak, current_streak)
+                    current_streak = 1
+
+        return max(longest_streak, current_streak)
+
+    # Time: O(N)
+    # Space: O(N)
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest_streak = 0
+        num_set = set(nums)
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
